143a01ba4dd2f534ff94e0a7ab30cb4c27cf02c4
CatalinaSorina <gherzan.catalina@yahoo.com>
2018-07-30 18:00:06 +0300

    Test DataOra in Main

---
+++ DataOra/src/datasiora/Main.java
@@ -0,0 +1,14 @@
+package datasiora;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		DataOra data1=new DataOra();
+		DataOra data2=new DataOra(14,0,0,29,7,2018);
+		System.out.println("Prima data creata automat:"+data1);
+		System.out.println("A doua data creata manual:"+data2);
+
+	}
+
+}

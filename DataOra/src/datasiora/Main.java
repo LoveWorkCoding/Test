5f22b3e864fab664d0199eee3ef785713541e4dc
CatalinaSorina <gherzan.catalina@yahoo.com>
2018-07-31 20:49:03 +0300

    test timp scurs

--- DataOra/src/datasiora/Main.java
+++ DataOra/src/datasiora/Main.java
@@ -8,6 +8,9 @@ public class Main {
 		DataOra data2=new DataOra(14,0,0,29,7,2018);
 		System.out.println("Prima data creata automat:"+data1);
 		System.out.println("A doua data creata manual:"+data2);
+		
+		String timpScurs=DataOra.timpScurs(data2, data1);
+		System.out.println(timpScurs);
 
 	}
 

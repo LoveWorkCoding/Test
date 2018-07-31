91160a88b53443cf862f68940244ebd61642ed3c
Sorin006 <37213944+Sorin006@users.noreply.github.com>
2018-07-31 21:36:48 +0300

    refacut222

--- DataOra/src/datasiora/Obiecte.java
+++ DataOra/src/datasiora/Obiecte.java
@@ -7,7 +7,7 @@ public class Obiecte {
 	private char obiect3;
 	
 	public Obiecte(String obiect1, int obiect2, char obiect3) {
-		super();
+
 		this.obiect1 = obiect1;
 		this.obiect2 = obiect2;
 		this.obiect3 = obiect3;
